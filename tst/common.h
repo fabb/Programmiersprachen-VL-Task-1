91c64bba08082cbf8bd67f1d47552ec9e3319edb
Bernhard Urban <lewurm@gmail.com>
2011-04-24 19:15:31 +0200

    example: ggt

--- tst/common.h
+++ tst/common.h
@@ -2,9 +2,24 @@
  * 1. arg: else
  * 2. arg: then
  * 3. arg: expr */
-#define IFTE [4c 5d 2+ d a]
+#define IFTE [4c 5d 2+ d a]a
 
 /* IF Else Then //cf. forth
  * TODO
  */
 #define IFET [4c 5d 2+ d a]
+
+/* swap the two upper elements */
+#define SWP 3c 4d
+
+/* duplicate top of stack */
+#define DUP 2c
+
+/* calcuate absolute value */
+/* pre: input */
+#define ABS DUP \
+	0 < \
+	[0 1 - * ] \
+	[] \
+	IFTE
+/* post: |input| */
